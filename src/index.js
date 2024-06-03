6fb72eaa5048313bdc56fa9328ddcd9d3a6d8fb2
alfmorais <alfredneto@hotmail.com>
2024-06-03 08:50:40 -0300

    feat: created router for list projects

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ app.get("/", function (request, response) {
     return response.json({ message: "Olá Dev! Bem vindo ao curso!" })
 })
 
+app.get("/projects", function (request, response) {
+    return response.json([
+        "Projeto 1",
+        "Projeto 2"
+    ])
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000!")
