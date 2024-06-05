da0af555bf75c8357059551d11544467ccbeb558
alfmorais <alfredneto@hotmail.com>
2024-06-05 09:03:05 -0300

    refactory: with projects array list

--- src/index.js
+++ src/index.js
@@ -1,44 +1,50 @@
 const express = require("express")
+const { v4: uuidv4 } = require("uuid")
+
 const app = express()
 
 app.use(express.json())
+
+const projects = []
 
 app.get("/", function (request, response) {
     return response.json({ message: "Olá Dev! Bem vindo ao curso!" })
 })
 
 app.get("/projects", function (request, response) {
-    const { title, owner, page } = request.query
-    console.log(title, owner, page)
-
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-    ])
+    return response.json(projects)
 })
 
 app.post("/projects", function (request, response) {
-    const body = request.body
-    console.log(body)
+    const { owner, name } = request.body
+    const project = {
+        id: uuidv4(),
+        name,
+        owner,
+    }
+    projects.push(project)
 
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-        "Projeto 3",
-        body,
-    ])
+    return response.status(201).json(project)
 })
 
 app.put("/projects/:id", function (request, response) {
     const { id } = request.params
     const { owner, name } = request.body
-    console.log(id, owner, name)
 
-    return response.json([
-        "Projeto 4",
-        "Projeto 2",
-        "Projeto 3",
-    ])
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        return response.status(404).json({ error: "Project not found" })
+    }
+
+    if (!owner || !name) {
+        return response.status(400).json({ error: "Owner and Name are required" })
+    }
+
+    const project = { id, name, owner }
+    projects[projectIndex] = project
+
+    return response.status(201).json(project)
 })
 
 app.delete("/projects/:id", function (request, response) {
