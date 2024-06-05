0979032f9f93158e56887492b33f31535a26723f
alfmorais <alfredneto@hotmail.com>
2024-06-05 08:40:21 -0300

    feat: introduction to body and params

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 app.get("/", function (request, response) {
     return response.json({ message: "Olá Dev! Bem vindo ao curso!" })
 })
 
 app.get("/projects", function (request, response) {
-    const {title, owner, page} = request.query
+    const { title, owner, page } = request.query
     console.log(title, owner, page)
 
     return response.json([
@@ -16,14 +18,22 @@ app.get("/projects", function (request, response) {
 })
 
 app.post("/projects", function (request, response) {
+    const body = request.body
+    console.log(body)
+
     return response.json([
         "Projeto 1",
         "Projeto 2",
         "Projeto 3",
+        body,
     ])
 })
 
 app.put("/projects/:id", function (request, response) {
+    const { id } = request.params
+    const { owner, name } = request.body
+    console.log(id, owner, name)
+
     return response.json([
         "Projeto 4",
         "Projeto 2",
