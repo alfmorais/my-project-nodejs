08d77a9c010700e24c1333e03ca38f247d2b6051
alfmorais <alfredneto@hotmail.com>
2024-06-05 14:53:50 -0300

    feat: introduction about middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ app.use(express.json())
 
 const projects = []
 
+function logRoutes(request, response, next) {
+    const { method, url } = request
+    const route = `[${method.toUpperCase()}] ${url}`
+    console.log(route)
+    return next()
+}
+
+app.use(logRoutes)
+
 app.get("/", function (request, response) {
     return response.json({ message: "Olá Dev! Bem vindo ao curso!" })
 })
@@ -48,10 +57,15 @@ app.put("/projects/:id", function (request, response) {
 })
 
 app.delete("/projects/:id", function (request, response) {
-    return response.json([
-        "Projeto 2",
-        "Projeto 3",
-    ])
+    const { id } = request.params
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        return response.status(404).json({ error: "Project not found" })
+    }
+
+    projects.splice(projectIndex, 1)
+    return response.status(204).send()
 })
 
 app.listen(3000, () => {
